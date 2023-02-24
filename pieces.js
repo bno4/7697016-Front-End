9fa865b9b36bf5fede61c199f0a2f6a45cf30c88
bno4 <bnduchemin@gmail.com>
2023-02-24 16:51:47 +0100

    exo

--- pieces.js
+++ pieces.js
@@ -1,31 +1,40 @@
+// Récupération des pièces depuis le fichier JSON
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 
-const article = pieces[1];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
+for (let i = 0; i < pieces.lenght; i++) {
 
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
+    const article = pieces[0];
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
 
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
 
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
 
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "(Pas de description pour le moment)";
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
 
-const disponibiliteElement = document.createElement("p");
-disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "(Pas de description pour le moment)";
 
-// document.body.appendChild(prixElement);
+    const disponibiliteElement = document.createElement("p");
+    disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
 
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(disponibiliteElement);
+    // document.body.appendChild(prixElement);
+    // Récupération de l'élément du DOM qui acceuillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création dune balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+
+    sectionFiches.appendChild(imageElement);
+    sectionFiches.appendChild(nomElement);
+    sectionFiches.appendChild(prixElement);
+    sectionFiches.appendChild(categorieElement);
+    sectionFiches.appendChild(descriptionElement);
+    sectionFiches.appendChild(disponibiliteElement);
+
+}
